09efbee09441038a6ac20747ef5aaef4531b42da
SirapobThongTham <154948323+SirapobThongTham@users.noreply.github.com>
2024-07-30 21:25:18 +0700

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styles from './Experience.module.css'
+import Photo from '/src/Photo/INTERNSHIP.jpg';
 
 const Experience = () => {
   return (
     <div id='Experience' className={styles.Experience_con}>
         <h1 className={styles.Experience_title}>My Experience</h1>
         <h2 className={styles.text_1}>EDUCATION INTERNSHIP</h2>
-        <img className={styles.img} src="src/Photo/INTERNSHIP.jpg" placeholder="NT Plc Kanchanaburi" />
+        <img className={styles.img} src={Photo} placeholder="NT Plc Kanchanaburi" />
         <h3 className={styles.text_2}>NT Plc Kanchanaburi</h3>
         <p>Internship period : 18 April 2023 to 19 June 2023<br />
           Building and Power Department</p>
